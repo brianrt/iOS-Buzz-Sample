775fbcfed3a9a4df28cbdf05b2d3201072b17d3e
brianrt <bthomp2000@gmail.com>
2020-11-22 23:18:06 -0600

    Initial Commit

---
+++ Text to Buzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text to Buzz
+//
+//  Created by Brian Thompson on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
